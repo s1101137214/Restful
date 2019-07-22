9cb4c4f345bd8a8548c078cbf981bc3203c472a1
Kent_Chou <kent_chou@bb.com>
2019-07-22 17:48:22 +0800

    remove comment

--- Server/server.js
+++ Server/server.js
@@ -7,28 +7,6 @@ router = new Router();
 
 var app = express();
 
-// server = Http.createServer(function (request, response) {
-//     router(request, response, function (error) {
-//         if (!error) {
-//             response.writeHead(404);
-//         } else {
-//             // Handle errors
-//             console.log(error.message, error.stack);
-//             response.writeHead(400);
-//         }
-
-//         response.writeHead(200, {
-//             'Content-Type': 'text/html'
-//         });
-//         response.write(Date() + "</br>");
-//         response.end('RESTful API Server is running!');
-//     });
-// });
-
-// server.listen(3000, function () {
-//     console.log('Listening on port 3000');
-// });
-
 //Json string.
 app.get("/", function (request, response) {
     response.json({
